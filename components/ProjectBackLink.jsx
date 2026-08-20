c071adbc467490649642bbcdf0cb46c0bec8d523
Yangl212 <yangl212@newschool.edu>
2026-08-20 16:14:23 -0400

    Update interest layout and header styling

--- components/ProjectBackLink.jsx
+++ components/ProjectBackLink.jsx
@@ -1,6 +1,8 @@
 "use client"
 
+import Image from "next/image"
 import Link from "next/link"
+import leftIcon from "../pic/Left.png"
 
 import styles from "./project-back-link.module.css"
 
@@ -9,7 +11,13 @@ export function ProjectBackLink({ href = "/project", className = "" }) {
     <div className={className}>
       <Link href={href} className={styles.backLink}>
         <span className={styles.arrow} aria-hidden="true">
-          {"<-"}
+          <Image
+            src={leftIcon}
+            alt=""
+            className={styles.arrowIcon}
+            width={28}
+            height={28}
+          />
         </span>
         <span>Back</span>
       </Link>
